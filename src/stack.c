8f6f753ccfe290186c0fd3be1cd5e6f2d2835e44
Sebastian Klang <sebklang@gmail.com>
2023-03-22 13:29:50 +0100

    stack.c rem 16 in macros

--- src/stack.c
+++ src/stack.c
@@ -3,12 +3,12 @@
 #include "emulator.h"
 #include "macros.h"
 
-#define POP16(X, Y) \
+#define POP(X, Y) \
     (X) = MEM[SP + 1]; \
     (Y) = MEM[SP]; \
     SP += 2
 
-#define PUSH16(X, Y) \
+#define PUSH(X, Y) \
     MEM[SP - 1] = (X); \
     MEM[SP - 2] = (Y); \
     SP -= 2
@@ -18,15 +18,15 @@ bool EmulateStack(EmulatorState *state, uint8_t *op)
     uint8_t temp;
 
     switch (*op) {
-    case 0xc1:  POP16(B, C); break;
-    case 0xc5: PUSH16(B, C); break;
-    case 0xd1:  POP16(D, E); break;
-    case 0xd5: PUSH16(D, E); break;
-    case 0xe1:  POP16(H, L); break;
+    case 0xc1:  POP(B, C); break;
+    case 0xc5: PUSH(B, C); break;
+    case 0xd1:  POP(D, E); break;
+    case 0xd5: PUSH(D, E); break;
+    case 0xe1:  POP(H, L); break;
     //   0xe3 (XHTL) handled below
-    case 0xe5: PUSH16(H, L); break;
-    case 0xf1:  POP16(A, FLAG_BYTE); break;
-    case 0xf5: PUSH16(A, FLAG_BYTE); break;
+    case 0xe5: PUSH(H, L); break;
+    case 0xf1:  POP(A, FLAG_BYTE); break;
+    case 0xf5: PUSH(A, FLAG_BYTE); break;
     case 0xf9: SP = HL;      break;
     case 0xe3: // XHTL
         temp = H;
