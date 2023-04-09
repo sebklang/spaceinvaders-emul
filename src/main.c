4b061122332c32fa4400ca63e8ade59c3394fda6
Sebastian Klang <sebklang@gmail.com>
2023-04-09 14:47:51 +0200

    fixed debug strings (culprit ptr cast) and more cli features

--- src/main.c
+++ src/main.c
@@ -14,15 +14,21 @@ int main(int argc, char *argv[])
     int filesize;
     uint8_t *memory;
     bool printState;
-    char dbgstrs[DEBUG_FRAME_LENGTH][64] = {0};
+    char  dbgstrs[DEBUG_FRAME_LENGTH][64] = {0};
+    char *dbgptrs[DEBUG_FRAME_LENGTH];
     EmulatorState state_;
     char disasmOut[64];
 
     EmulatorState *state = &state_;
     int  const memsize   = 1 << 16;
     char const *const fn = "bin/invaders.rom";
-    DebugFrame dbg_ = {(char **) dbgstrs, DEBUG_FRAME_LENGTH, 0};
+    DebugFrame dbg_ = {(char **) dbgptrs, DEBUG_FRAME_LENGTH, 0};
     DebugFrame *dbg = &dbg_;
+
+    // Initialize dbgptrs. (Do not use dbg_ or dbg before this)
+    for (int i = 0; i < DEBUG_FRAME_LENGTH; i++) {
+        dbgptrs[i] = dbgstrs[i];
+    }
 
     // Initialize memory from ROM file
     if ((file = fopen(fn, "rb")) == NULL) {
@@ -31,10 +37,10 @@ int main(int argc, char *argv[])
     }
     fseek(file, 0L, SEEK_END);
     filesize = ftell(file);
+    fseek(file, 0L, SEEK_SET);
     memory = malloc(memsize);
     fread(memory, filesize, 1, file);
     fclose(file);
-    fseek(file, 0L, SEEK_SET);
     // -----
 
     InitEmulator(state, memory, memsize, NULL, NULL);
@@ -44,18 +50,22 @@ int main(int argc, char *argv[])
         // Output
         if (printState) {
             for (int i = 0; i < 10; i++) printf("\n");
-            printf("Current state:\nA = %02x\nBC = %02x %02x\n", A, B, C);
-            printf("DE = %02x %02x\nHL = %02x %02x\n", D, E, H, L);
-            printf("SP = %02x %02x\n", SP_HI, SP_LO);
-            printf("PC = %02x %02x\n", PC_HI, PC_LO);
-            printf("Flag byte = %02x\n", FLAG_BYTE);
-            printf("    Sign = %d\n    Zero = %d\n    ----\n", FLAG_S, FLAG_Z);
-            printf("    Aux Carry = %d\n    ----\n    Parity = %d\n", FLAG_AC, FLAG_P);
-            printf("    ----\n    Carry = %d\n", FLAG_C);
-            printf("Halted = %d\nIrq Enable = %d\n\n", HALTED, INT_ENABLE);
+            printf("Current state:\n"
+            "A = %02x\nBC = %02x %02x\n"
+            "DE = %02x %02x\nHL = %02x %02x\n"
+            "SP = %02x %02x\nPC = %02x %02x\n"
+            "Flag byte = %02x\n"
+            "    Sign = %d\n    Zero = %d\n    ----\n"
+            "    Aux Carry = %d\n    ----\n    Parity = %d\n"
+            "    ----\n    Carry = %d\n"
+            "Halted = %d\nIrq Enable = %d\n\n"
+            "Type the number of instructions to execute,\n"
+            "or press enter to execute the next instruction.\n"
+            "Next instruction to be executed is:\n",
+            A, B, C, D, E, H, L, SP_HI, SP_LO, PC_HI, PC_LO,
+            FLAG_BYTE, FLAG_S, FLAG_Z, FLAG_AC, FLAG_P, FLAG_C,
+            HALTED, INT_ENABLE);
 
-            printf("Type the number of instructions to execute,\nor press enter to execute the next instruction.\n");
-            printf("Next instruction to be executed is:\n");
             DisasmSingleInstruction(disasmOut, memory, PC);
             printf("%s", disasmOut); 
         }
@@ -70,12 +80,19 @@ int main(int argc, char *argv[])
             numIter = 1;
         }
         else if (strcmp(inString, "ps\n") == 0) {
-            printf("--- Stack ---\n");
-            printf("Address | Value\n");
+            printf("--- Stack ---\nAddress | Value\n");
             for (int i = 0x23ff; i >= SP; i--) { // 0x2400 is specific to space invaders
                 printf("%04x    | %02x\n", i, *GetMem(state, i));
             }
             printf("--- End of stack ---\n");
+        }
+        else if (strcmp(inString, "pf\n") == 0) {
+            printf("Printing last up to %d executed instructions.\n", DEBUG_FRAME_LENGTH);
+            PrintFrame(dbg);
+        }
+        else if (strcmp(inString, "q\n") == 0) {
+            printf("Exiting.\n");
+            return 0;
         }
         else {
             numIter = atoi(inString);
